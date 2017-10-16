ce4b34918b1e91abc5c3e7d44ce2f44eedbc152f
Shane <sdarcy@redventures.com>
2017-10-16 12:24:05 -0400

    pulling data in express

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
-  database : 'BreweryTapHandles'
+  database : 'BreweryTapHandles',
+  port: 8889
 });
 
-connection.connect((error) => {
-    if(!!error){
+connection.connect((err) => {
+    if(!!err){
         console.log("Error connecting to Shane's Database");
     } else {
         console.log("Connected to Shane's Database");
@@ -25,34 +26,21 @@ const app = express();
 // app.use(express.bodyParser());
 
 app.get('/', (req, res) => {
-    connection.query("SELECT * FROM TapHandles", (err, rows, fields) => {
+    connection.query("SELECT * FROM TapHandles", (err, result) => {
         if(!!err){
             console.log('Error in the query');
         } else {
             console.log('Successful query');
+            console.log(result);
+            res.send('Database created...');
         }
     });
 });
-
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//   if (err) throw err
-//   console.log('The solution is: ', rows[0].solution)
-// });
-
-// app.get("/",function(req,res){
-//     connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
-//     connection.end();
-//       if (!err)
-//         console.log('The solution is: ', rows);
-//       else
-//         console.log('Error while performing Query.');
-//       });
-//     });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(5000, () => {
+app.listen('5000', () => {
   console.log('The Beer Taps Ecommerce site is running on Port 5000!')
 });
