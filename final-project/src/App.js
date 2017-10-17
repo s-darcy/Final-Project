5c5ddc4646fac08408d5d03549546f4017e0b3d3
Shane <sdarcy@redventures.com>
2017-10-17 01:02:28 -0400

    fixed CSS Layout

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -10,19 +10,43 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <img alt="21st Amendment Blah, Blah, Blah Tap Handle" src="../TapHandles/21stAmendment.JPG" />
-        <img alt="Terrapin Hopsecutioner Tap Handle" src="../TapHandles/Terrapin.JPG" />
-        <img alt="Wicked Weed Freak of Nature Tap Handle" src="../TapHandles/WickedWeed.JPG" class="wickedWeed" />
-        <img alt="Coast Brewing Tap Handle" src="../TapHandles/Coast.JPG" />
-        <img alt="The Unknown Brewing Tap Handle" src="../TapHandles/Unknown.JPG" />
-        <img alt="Sweet Water 420 Tap Handle" src="../TapHandles/SweetWater.JPG" />
-        <img alt="Legal Remedy Brewing Tap Handle" src="../TapHandles/LegalRemedy.JPG" />
-        <img alt="Stone IPA Tap Handle" src="../TapHandles/Stone.JPG" />
-        <img alt="Sierra Nevada Torpdeo Tap Handle" src="../TapHandles/SierraNevada.JPG" />
-        <img alt="Scofflaw Brewing Tap Handle" src="../TapHandles/Scofflaw.JPG" />
-        <img alt="Triple C Brewing Tap Handle" src="../TapHandles/TripleC.JPG" />
-        <img alt="NoDa Brewing Tap" src="../TapHandles/NoDa.JPG" />
+      <div className="App products">
+        <div>
+          <img alt="21st Amendment Blah, Blah, Blah Tap Handle" src="../TapHandles/21stAmendment.JPG" />
+        </div>
+        <div>
+          <img alt="Terrapin Hopsecutioner Tap Handle" src="../TapHandles/Terrapin.JPG" />
+        </div>
+        <div>
+          <img alt="Wicked Weed Freak of Nature Tap Handle" src="../TapHandles/WickedWeed.JPG" className="wickedWeed" />
+        </div>
+        <div>
+          <img alt="Coast Brewing Tap Handle" src="../TapHandles/Coast.JPG" />
+        </div>
+        <div>
+          <img alt="The Unknown Brewing Tap Handle" src="../TapHandles/Unknown.JPG" />
+        </div>
+        <div>
+          <img alt="Sweet Water 420 Tap Handle" src="../TapHandles/SweetWater.JPG" />
+        </div>
+        <div>
+          <img alt="Legal Remedy Brewing Tap Handle" src="../TapHandles/LegalRemedy.JPG" />
+        </div>
+        <div>
+          <img alt="Stone IPA Tap Handle" src="../TapHandles/Stone.JPG" />
+        </div>
+        <div>
+          <img alt="Sierra Nevada Torpdeo Tap Handle" src="../TapHandles/SierraNevada.JPG" />
+        </div>
+        <div>
+          <img alt="Scofflaw Brewing Tap Handle" src="../TapHandles/Scofflaw.JPG" />
+        </div>
+        <div>
+          <img alt="Triple C Brewing Tap Handle" src="../TapHandles/TripleC.JPG" />
+        </div>
+        <div>
+          <img alt="NoDa Brewing Tap" src="../TapHandles/NoDa.JPG" />
+        </div>
       </div>
     );
   }
