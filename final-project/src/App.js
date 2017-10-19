7a45b429a4030fceb0fe34b97d71b37623adc900
Shane <sdarcy@redventures.com>
2017-10-19 16:53:41 -0400

    got it working

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -8,15 +8,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      availableProducts :[],
+      availableProducts : [],
       products : [],
       quantity : [],
+      value: '',
 
       title: 'Craft Beer Tap Handles Store'
     };
     this.fetchProducts = this.fetchProducts.bind(this);
     this.handleQuantity = this.handleQuantity.bind(this);
     this.handleProducts = this.handleProducts.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     this.fetchProducts();
   }
@@ -24,10 +26,19 @@ class App extends Component {
   handleQuantity(event) {
     event.preventDefault();
     let quantitySelected = event.target.value;
+
     this.state.quantity.push(quantitySelected);
-    this.setState({value: quantitySelected});
+    this.setState({
+      value: quantitySelected
+    });
+
     console.log("You have added " + quantitySelected);
     console.log(this.state.quantity);
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
   }
 
   handleProducts(event) {
@@ -50,10 +61,12 @@ class App extends Component {
         <ProductInfo
           key={i}
           product={availableProducts}
+          value={this.state.value}
+          handleQuantity = {this.handleQuantity}
         /> 
-      )
-    }, this);
-    
+      );
+    }, this); 
+
     return (
 
       <div className="App">
@@ -68,57 +81,8 @@ class App extends Component {
         </ul>
         <div className="products"> 
           {eachProduct}
-          <div>
-            <img alt="Sierra Nevada Torpdeo Tap Handle" src="img/SierraNevada.JPG" />
-          </div>
-          <div>
-            <img alt="Sweet Water 420 Tap Handle" src="img/SweetWater.JPG" />
-          </div>
-          <div>
-            <img alt="Coast Brewing Tap Handle" src="img/Coast.JPG" />
-          </div>
-          <div>
-            <form>
-              <button name="21st Amendment Blah, Blah, Blah Tap Handle" onClick={this.handleProducts}>
-                <h3>21st Amendment Blah, Blah, Blah Tap Handle</h3>
-                <img onClick={this.handleProducts} alt="21st Amendment Blah, Blah, Blah Tap Handle" src="img/21stAmendment.JPG" />
-              </button>
-              <select value={this.state.value} onChange={this.handleQuantity}>
-                <option value="null">--</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </form>  
-          </div>
-          <div>
-            <img alt="Terrapin Hopsecutioner Tap Handle" src="img/Terrapin.JPG" />
-          </div>
-          <div>
-            <img alt="Stone IPA Tap Handle" src="img/Stone.JPG" />
-          </div>
-          <div>
-            <img alt="Legal Remedy Brewing Tap Handle" src="img/LegalRemedy.JPG" />
-          </div>
-          <div>
-            <img alt="Wicked Weed Freak of Nature Tap Handle" src="img/WickedWeed.JPG" className="wickedWeed" />
-          </div>
-          <div>
-            <img alt="The Unknown Brewing Tap Handle" src="img/Unknown.JPG" />
-          </div>
-          <div>
-            <img alt="Triple C Brewing Tap Handle" src="img/TripleC.JPG" />
-          </div>
-          <div>
-            <img alt="Scofflaw Brewing Tap Handle" src="img/Scofflaw.JPG" />
-          </div>
-          <div>
-            <img alt="NoDa Brewing Tap" src="img/NoDa.JPG" />
-          </div>
         </div>
-      </div> //App
+      </div> 
     )
   }
 
@@ -134,6 +98,5 @@ class App extends Component {
     });
   };
 }
-
 
 export default App;
--- final-project/src/ProductInfo.js
+++ final-project/src/ProductInfo.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 class ProductInfo extends Component {
+
     render(){
+
       return (
         <div key={this.props.product.ProductID}>
+        <button name={this.props.product.Name} onClick={this.handleProducts}>
           <h3 >{this.props.product.Name}</h3>
+          <img alt={this.props.product.Name} src={process.env.PUBLIC_URL + this.props.product.Images} /> 
           <p>{this.props.product.Description}</p>
-          <p>${this.props.product.Price}.00</p> 
+          <p>${this.props.product.Price}.00</p>
+          <form name="myForm" onSubmit={this.handleSubmit}>
+              <label>
+                Select the number of beer taps that you would like to order:
+                <select value={this.props.value} onChange={this.props.handleQuantity}>
+                  <option value="0">--</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </label>  
+              <input type="submit" value="Submit" />
+          </form> 
+          </button> 
         </div>
       );
     }     
